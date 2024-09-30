c444be1dc0e2f46b4b66995cbc5cf3b5b6845045
Manas Pratim Kundu <142176832+pratim808@users.noreply.github.com>
2024-09-30 17:13:17 +0530

    Update ocr_utils.py

--- ocr_utils.py
+++ ocr_utils.py
@@ -21,14 +21,3 @@ def extract_text(image):
 
     return response.text
 
-
-def highlight_content(full_text, keyword):
-    if keyword:
-        # Highlight the keyword in the text
-        highlighted_text = full_text.replace(
-            keyword, f"<span class='highlight'>{keyword}</span>"
-        )
-
-        return highlighted_text
-    else:
-        return "No keyword entered for highlighting."
